9be7f6cd718ab176e66518c2b81660ec57dec702
liuxiao <xiao.liu@dragonflow.com>
2017-06-29 09:27:43 +0800

    update

---
+++ chat_sys_v3.9/pages/office_automation/supervision/flowTrace_comp.jsx
@@ -0,0 +1,52 @@
+import $ from 'jquery';
+import React from 'react';
+import { ListView, List,NavBar} from 'antd-mobile';
+import { Icon} from 'antd';
+import CommonFlowTraceComp from '../common_flowTrace_comp.jsx';
+
+//签报管理的办文跟踪组件。
+class SuperviseFlowTraceComp extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+      };
+  }
+  componentWillMount(){
+  }
+  onNavBarLeftClick = (e) => {
+    this.props.backDetailCall();
+  }
+  render() {
+
+    return (
+      <div>
+        <NavBar className="mobile_navbar_custom"
+          style={{position:'fixed',height:'60px',zIndex:'13',width:'100%',top:0}}
+          iconName = {false} onLeftClick={this.onNavBarLeftClick}
+          leftContent={[
+            <Icon type="arrow-left" className="back_arrow_icon" key={2}/>,
+            <span key={1}>返回</span>
+          ]} >
+          办文跟踪
+        </NavBar>
+        <div style={{marginTop:'60px'}}>
+          <CommonFlowTraceComp
+            tokenunid={this.props.tokenunid}
+            docunid={this.props.docunid}
+            gwlcunid={this.props.gwlcunid}
+            modulename={this.props.modulename}
+            />
+        </div>
+      </div>
+    )
+  }
+}
+
+SuperviseFlowTraceComp.defaultProps = {
+};
+
+SuperviseFlowTraceComp.propTypes = {
+  backDetailCall:React.PropTypes.func,
+};
+
+export default SuperviseFlowTraceComp;
